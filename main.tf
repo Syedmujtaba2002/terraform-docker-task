6273551a2772851465511cab05057d9ff8aaf0d8
Syedmujtaba2002 <syedmujtabahussaini03@gmail.com>
2025-08-08 00:45:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,16 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "node_app_image" {
-  name = "syedmujtaba2002/nodejs-demo-app:latest"
+resource "docker_image" "app_image" {
+  name = var.docker_image_name
 }
 
-resource "docker_container" "node_app_container" {
-  name  = "nodejs-demo-app"
-  image = docker_image.node_app_image.name
+resource "docker_container" "app_container" {
+  name  = var.container_name
+  image = docker_image.app_image.name
+
   ports {
-    internal = 3000    # Change to your app's container port if needed
-    external = 8080    # Host port to access via browser
+    internal = var.container_port
+    external = var.host_port
   }
 }
